cb140b10a4f9ffc7681a2624f850f5b66eacda13
Andrew Wang <andrewawang@berkeley.edu>
2017-12-30 17:17:23 -0800

    Spiffied up

--- stupid_password_hasher.py
+++ stupid_password_hasher.py
@@ -1,32 +1,62 @@
 import bcrypt as bc
 import getpass as gp
+import random
+import string
 
-password = gp.getpass("Password to be hashed: ").encode('utf8')
-hashed = bc.hashpw(password, bc.gensalt()).decode("utf-8")
+run = True
 
-def num(s):
-    try:
-        s = int(s)
-        if s > 53:
-            return big(s)
-        if s < 1:
-            return small(s)
-        return s
+def main():
+    while(run):
+        generate()
+        stop()
+
+def stop():
+    yn = input("Continue? [y/n]: ").lower()
+
+    if (yn == 'n' or yn == 'no'):
+        global run
+        run = False
+
+    elif (yn != 'y' and yn != 'yes'):
+        stop()
+
+def generate():
+    password = gp.getpass("Password to be hashed: ").encode('utf8')
+
+    seed = gp.getpass("Salt seed: ")
+    random.seed(seed)
+
+    salt = '$2b$05$' + ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k = 22))
+    salt = salt.encode('utf8')
+
+
+    hashed = bc.hashpw(password, salt).decode("utf-8")
+
+    def num(s):
+        try:
+            s = int(s)
+            if s > 32:
+                return big(s)
+            if s < 1:
+                return small(s)
+            return s
     
-    except ValueError:
-        s = input("Input an integer value: ")
-        return num(s)
+        except ValueError:
+            s = input("Input an integer value: ")
+            return num(s)
     
-def big(i):
-    while (i > 53):
-        i = num(input("Input an integer value less than 54: "))
-    return i
+    def big(i):
+        if (i > 32):
+            i = num(input("Input an integer value less than 33: "))
+        return i
 
-def small(i):
-    while (i < 1):
-        i = num(input("Input an integer value greater than 0: "))
-    return i
+    def small(i):
+        if (i < 1):
+            i = num(input("Input an integer value greater than 0: "))
+        return i
 
-length = num(input("Length of hash: "))
-print("Hash: " + hashed[7:length + 7])
+    length = num(input("Length of hash: "))
+    print("Hash: " + hashed[28 : length + 28])
 
+
+main()    
